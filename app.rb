69b09f8b3f45a6643227613091daf8a52cf4b6e3
srappaport <sarah.rappaport@greendot.org>
2018-05-31 04:09:22 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,8 +2,8 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 
-  get '/' do 
-    "Welcome to your app!!!! hiiii"
+  get '/' do
+    "Welcome to your app!!!! hiiii hola TURTLE"
   end
 
 end
